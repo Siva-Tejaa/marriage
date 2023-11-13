3f9af06bce7d7aa6c03ff239411549aeb1d1d819
Siva-Tejaa <asivateja1999@gmail.com>
2023-11-13 20:18:35 +0530

    date design added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,44 +1,24 @@
 import React from 'react';
 
 import Marriage_Banner from "../../assets/Marriage_Banner.jpg"
+import Quotation from '../Quotation/Quotation';
 
 const Body = () => {
 
-  const downloadICS = () => {
-    const event = {
-      summary: 'Event Summary',
-      location: 'Event Location',
-      description: 'Event Description',
-      start: '20231115T120000', // Format: YYYYMMDDTHHmmss
-      end: '20231115T140000',   // Format: YYYYMMDDTHHmmss
-    };
-
-    const calendarEvent = [
-      'BEGIN:VCALENDAR',
-      'VERSION:2.0',
-      'BEGIN:VEVENT',
-      `SUMMARY:${event.summary}`,
-      `LOCATION:${event.location}`,
-      `DESCRIPTION:${event.description}`,
-      `DTSTART:${event.start}`,
-      `DTEND:${event.end}`,
-      'END:VEVENT',
-      'END:VCALENDAR',
-    ].join('\n');
-
-    const element = document.createElement('a');
-    const file = new Blob([calendarEvent], { type: 'text/calendar' });
-    element.href = URL.createObjectURL(file);
-    element.download = 'event.ics';
-    element.click();
-  };
-
   return (
-    <div className='text-white text-center flex flex-col items-center justify-end bg-[center_center] bg-no-repeat bg-[initial] bg-cover h-[100vh]' style={{backgroundImage: `url(${Marriage_Banner})`, textShadow: '1px 1px 3px #0e0e0e'}}>
-      <p>Wedding Invitation For</p>
-      <p className='text-3xl laptop:text-4xl' style={{ fontFamily: "'Dancing Script', cursive" }}>Virat Kohli & Anushka Sharma</p>
-      <p className='font-medium text-xl mb-24' style={{ fontFamily: "'Signika Negative', sans-serif" }}>12 - 04 - 2026</p>
-    </div>
+    <>
+      <div className='text-white text-center flex flex-col items-center justify-end bg-[center_center] bg-no-repeat bg-[initial] bg-cover h-[100svh]' style={{backgroundImage: `url(${Marriage_Banner})`, textShadow: '1px 1px 3px #0e0e0e'}}>
+        <p>Wedding Invitation For</p>
+        <p className='text-3xl laptop:text-4xl' style={{ fontFamily: "'Dancing Script', cursive" }}>Virat Kohli & Anushka Sharma</p>
+        <div className='flex items-center justify-center mb-20 mt-2'>
+          <img src="https://themes.sindevo.com/wedding-bells/images/swirl_left.png" alt="LeftDesign" style={{filter:'drop-shadow(1px 1px 3px #0e0e0e)'}}/>
+          <p className='font-medium text-xl px-4' style={{ fontFamily: "'Signika Negative', sans-serif" }}>12.04.2026</p>
+          <img src="https://themes.sindevo.com/wedding-bells/images/swirl_right.png" alt="RightDesign" style={{filter:'drop-shadow(1px 1px 3px #0e0e0e)'}}/>
+        </div>
+      </div>
+      <Quotation/>
+    </>
+    
   )
 }
 
---
+++ src/components/Quotation/Quotation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Quotation = () => {
+  return (
+    <div className='bg-[#f5dcb2] flex items-center justify-center p-2'>Quotation</div>
+  )
+}
+
+export default Quotation
