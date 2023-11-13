3eb0a51ba7919f8ac6ecde4c96bc427885bb792d
Siva-Tejaa <asivateja1999@gmail.com>
2023-11-13 19:41:27 +0530

    img height adjusted

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -34,10 +34,10 @@ const Body = () => {
   };
 
   return (
-    <div className='text-white text-center flex flex-col items-center justify-end bg-[center_center] bg-no-repeat bg-[initial] bg-cover h-[37rem]' style={{backgroundImage: `url(${Marriage_Banner})`, textShadow: '1px 1px 3px #0e0e0e'}}>
+    <div className='text-white text-center flex flex-col items-center justify-end bg-[center_center] bg-no-repeat bg-[initial] bg-cover h-[100vh]' style={{backgroundImage: `url(${Marriage_Banner})`, textShadow: '1px 1px 3px #0e0e0e'}}>
       <p>Wedding Invitation For</p>
       <p className='text-3xl laptop:text-4xl' style={{ fontFamily: "'Dancing Script', cursive" }}>Virat Kohli & Anushka Sharma</p>
-      <p className='font-medium text-xl mb-32' style={{ fontFamily: "'Signika Negative', sans-serif" }}>12 - 04 - 2026</p>
+      <p className='font-medium text-xl mb-24' style={{ fontFamily: "'Signika Negative', sans-serif" }}>12 - 04 - 2026</p>
     </div>
   )
 }
