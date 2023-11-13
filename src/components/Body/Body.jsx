45b0d603a707c54ab9e7fcc98a079255a04b8ed5
Siva-Tejaa <asivateja1999@gmail.com>
2023-11-13 23:53:23 +0530

    bottom nav added styles

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -12,7 +12,7 @@ const Body = () => {
     <>
       <div className='text-white text-center flex flex-col items-center justify-end bg-[center_center] bg-no-repeat bg-[initial] bg-cover h-[100svh] p-2' style={{backgroundImage: `url(${Marriage_Banner})`, textShadow: '1px 1px 3px #0e0e0e'}}>
         <p>Wedding Invitation For</p>
-        <p className='text-3xl laptop:text-4xl' style={{ fontFamily: "'Dancing Script', cursive" }}>Virat Kohli ❤️ Anushka Sharma</p>
+        <p className='text-3xl laptop:text-4xl' style={{ fontFamily: "'Dancing Script', cursive" }}>Virat Kohli & Anushka Sharma</p>
         <div className='flex items-center justify-center mb-20 mt-2'>
           <img src={left_date_design} alt="LeftDesign" style={{filter:'drop-shadow(1px 1px 3px #0e0e0e)'}}/>
           <div className='flex items-center justify-center flex-col'>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { AiTwotoneHome } from "react-icons/ai";
 import { BsCalendar2EventFill } from "react-icons/bs";
 import { MdPeopleAlt } from "react-icons/md";
@@ -6,29 +6,48 @@ import { IoMdPhotos } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 
 const Header = () => {
+
+    const navItems = [
+        {
+            id:1,
+            name:"Home",
+            icon:<AiTwotoneHome fontSize="1.3em"/>
+        },
+        {
+            id:2,
+            name:"Events",
+            icon:<BsCalendar2EventFill fontSize="1.2em"/>
+        },
+        {
+            id:3,
+            name:"Contact",
+            icon:<MdPeopleAlt fontSize="1.4em"/>
+        },
+        {
+            id:4,
+            name:"Gallery",
+            icon:<IoMdPhotos fontSize="1.3em"/>
+        },
+        {
+            id:5,
+            name:"Venue",
+            icon:<FaLocationDot fontSize="1.3em"/>
+        }
+    ]
+    
+    const[activenav, setActiveNav] = useState("Home");
+
   return (
-    <header className='fixed bottom-4 text-slate-400 flex items-center justify-center w-full' style={{filter:'drop-shadow(1px 1px 3px #0e0e0e)'}}>
-        <nav className='flex items-center justify-between gap-5 bg-white p-3 pb-1 px-4 rounded-full'>
-            <div className='flex flex-col items-center justify-center'>
-                <AiTwotoneHome/>
-                <p className='text-sm'>Home</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <BsCalendar2EventFill/>
-                <p className='text-sm'>Events</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <MdPeopleAlt/>
-                <p className='text-sm'>Contact</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <IoMdPhotos/>
-                <p className='text-sm'>Gallery</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <FaLocationDot/>
-                <p className='text-sm'>Venue</p>
-            </div>
+    <header className='fixed bottom-4 flex items-center justify-center w-full' style={{filter:'drop-shadow(1px 1px 3px #0e0e0e)'}}>
+        <nav className='flex items-center justify-between gap-5 bg-white px-[1.20rem] pt-2 pb-1 rounded-full'>
+            {
+                navItems.map((nav) => (
+                    <div key={nav?.id} className={activenav === nav?.name ? 'flex flex-col items-center justify-center cursor-pointer text-[#d19342]' : 'flex flex-col items-center justify-center cursor-pointer text-[#767676]'} onClick={() => setActiveNav(nav?.name)}>
+                        {nav?.icon}
+                        <p className='text-sm'>{nav?.name}</p>
+                    </div>
+                ))
+            }
         </nav>
     </header>
   )
